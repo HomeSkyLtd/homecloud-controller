a197dfb73190d1aed541ecb159de7d8e32d3e1c7
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-09-15 12:51:13 -0300

    last fixes

--- homecloud.js
+++ homecloud.js
@@ -436,6 +436,7 @@ Homecloud.prototype._sendMessage = function (message, events) {
     }, (data, response) => {
         if (data.status !== 200) {
             console.log("[MESSAGE] Non 200 status received (" + data.status + ") ");
+            console.log(message);
             if (data.errorMessage)
                 console.log("[MESSAGE] Error: " + data.errorMessage);
             if (data.status === 403 && data.errorMessage === 'User not logged in') {
@@ -449,6 +450,7 @@ Homecloud.prototype._sendMessage = function (message, events) {
         }
         else {
             console.log("[MESSAGE] Sent Message");
+            console.log(message);
             if (events.onSend)
                 events.onSend(data);
         }
